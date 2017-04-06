1a4c38f0ca3f1a2c6da042e9dc2cfbeac81218f4
Quinn <qmm25@drexel.edu>
2017-04-06 12:50:17 -0400

    initial commit

---
+++ ImageImport/Assets/scripts/ImageSequenceSingleTexture.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageSequenceSingleTexture : MonoBehaviour 
+    {
+    //texture object that will output animation, gameobject's material, frames
+    private Texture texture;
+    private Material goMaterial;
+    private int frameCounter = 0;
+    
+
+    //
+    public string folderName;
+    public string imageSequenceName;
+    //number of frames in animation - the t value from images
+    public int numberofFrames;
+    //base name of files in sequence
+    private string baseName;
+
+
+    private void Awake()
+    {
+        goMaterial = GetComponent<Material>();
+        
+        baseName = folderName + "/" + imageSequenceName;
+    }
+    
+	// Use this for initialization
+	void Start ()
+    {
+        texture = (Texture)Resources.Load(baseName + "0", typeof(Texture));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //start 'playloop' method as coroutine with 0.04 delay
+        StartCoroutine("PlayLoop", 0.04f);
+        //set materials texcture to current value of frameCount
+        goMaterial.mainTexture = texture; 
+	}
+
+    IEnumerator PlayLoop(float delay)
+    {
+        //wait for the time defined at delay param
+        yield return new WaitForSeconds(delay);
+        //advance one frame
+        frameCounter = (++frameCounter) % numberofFrames;
+        //load current frame
+        texture = (Texture)Resources.Load(baseName + frameCounter.ToString("D5"), typeof(Texture));
+        //stop coroutine
+        StopCoroutine("PlayLoop");
+    }
+}
---
+++ ImageImport/Assets/scripts/InstantiatePlanes.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using System.IO;
+
+public class InstantiatePlanes : MonoBehaviour {
+
+    
+    public Transform zPlane;
+    public float zValue = 0.000981f;
+
+    //public string folderName;
+    //public string imageSequenceName;
+    //private string baseName;
+
+    public Material newMaterial;
+    public Texture[] newTexture;
+    
+
+    /*
+    //get number of images in folder
+    public static long DirCount(DirectoryInfo d)
+    {
+        long i = 0;
+        // Add file sizes.
+        FileInfo[] fis = d.GetFiles();
+        foreach (FileInfo fi in fis)
+        {
+            if (fi.Extension.Equals(".tif", StringComparison.OrdinalIgnoreCase))
+                i++;
+        }
+        return i;
+        
+
+    }*/
+
+    private void Awake()
+    {
+        //newMaterial = GetComponent<Material>();
+        //baseName = folderName + "/" + imageSequenceName;
+    }
+    
+    // Use this for initialization
+    void Start () {
+        for (int i= 0; i<= 24; i++)
+        {
+            float newzValue = zValue*i;
+            //Debug.Log(newzValue);
+            Texture newMat = newMaterial.mainTexture = newTexture[i];
+            Instantiate(zPlane, new Vector3(0,newzValue,0), Quaternion.identity);
+            
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
