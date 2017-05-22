1bbc42c57daed9948ecc5148a984403d8ec80cb2
Quinn <qmm25@drexel.edu>
2017-05-22 16:44:23 -0400

    no message

---
+++ ImageImport/Assets/scripts/AnimatePlanes.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatePlanes : MonoBehaviour {
+    public Texture[] textures;
+    public int currentTexture;
+    new Renderer renderer;
+	// Use this for initialization
+	void Start () {
+        renderer = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+
+            currentTexture++;
+            currentTexture %= textures.Length;
+            renderer.material.mainTexture = textures[currentTexture];
+            Debug.Log(currentTexture);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            currentTexture--;
+            renderer.material.mainTexture = textures[currentTexture];
+            Debug.Log(currentTexture);
+            
+
+        }
+    }
+}
