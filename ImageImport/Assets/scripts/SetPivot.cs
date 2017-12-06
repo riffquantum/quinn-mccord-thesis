d441785bb43e723d4de05de26877c1501fa73a58
quinn <qmm25@drexel.edu>
2017-12-06 16:26:00 -0500

    removed finger colliders, began implementing pivot point around grabbing controller for more accurate rotation

---
+++ ImageImport/Assets/scripts/SetPivot.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPivot : MonoBehaviour {
+    public Transform PivotPointLocation;
+    enum Controller { LTouch, RTouch };
+    
+    public Vector3 LControllerPosition;
+    public Vector3 RControllerPosition;
+
+    
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        LControllerPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTouch);
+        RControllerPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
+    }
+
+    public void LeftSetPivotLocation()
+    {
+        if (OVRInput.Axis1D.SecondaryHandTrigger == 0)
+        {
+            PivotPointLocation.transform.position = LControllerPosition;
+            Debug.Log(PivotPointLocation.transform.position);
+        }
+        
+    }
+    public void RightSetPivotLocation()
+    {
+        if (OVRInput.Axis1D.PrimaryHandTrigger == 0)
+        {
+            PivotPointLocation.transform.position = RControllerPosition;
+            Debug.Log(PivotPointLocation.transform.position);
+        }
+        
+    }
+
+    public void Release()
+    {
+        Debug.Log("Released");
+    }
+
+}
+
